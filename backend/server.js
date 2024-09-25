72b72665789731fc47f7db7359419256e35ce2bc
kevin young <you.k2208@gmail.com>
2024-09-25 16:55:18 -0700

    imported connectMongoDB function to server js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongDB.js";
 
 dotenv.config();
 
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
   console.log("Servier is running on port 8000");
+  connectMongoDB();
 });

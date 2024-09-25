fe46a78a559d11763db633b96e3c69cdd9786399
kevin young <you.k2208@gmail.com>
2024-09-25 16:20:29 -0700

    created auth signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+//middleware
+app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
   console.log("Servier is running on port 8000");

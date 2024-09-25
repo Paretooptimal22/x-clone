a3b80c5978abbedad99e0c2c8fdd110ad27f891c
kevin young <you.k2208@gmail.com>
2024-09-25 16:22:30 -0700

    added login and logout endpoints

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -8,4 +8,16 @@ router.get("/signup", (req, res) => {
   });
 });
 
+router.get("/login", (req, res) => {
+  res.json({
+    data: "You hit the login endpoint",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  res.json({
+    data: "You hit the logout endpoint",
+  });
+});
+
 export default router;

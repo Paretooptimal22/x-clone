36c671eececfabbb2c5c37695bb2aef1a10b38b8
kevin young <you.k2208@gmail.com>
2024-09-26 12:43:05 -0700

    updated user controller with followUnfollow function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,12 +5,45 @@ export const getUserProfile = async (req, res) => {
 
   try {
     const user = await User.findOne({ username }).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    if (!user) return res.status(404).json({ message: "User not found" });
+
     res.status(200).json(user);
   } catch (error) {
     console.log("Error in getUserProfile: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const followUnfollowProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: "You can't follow/unfollow yourself" });
+
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: "User not found" });
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      //unfollow the user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      //follow the user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      //send notification to the user
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    console.log("Error in followUnfollowProfile: ", error.message);
     res.status(500).json({ error: error.message });
   }
 };

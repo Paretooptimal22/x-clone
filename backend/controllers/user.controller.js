2068c24ae2653db91ae0ee616a2316a869bc59b1
kevin young <you.k2208@gmail.com>
2024-09-26 14:39:06 -0700

    added if checks for updating user password

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -85,3 +85,40 @@ export const getSuggestedUsers = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  const { fullName, email, username, currentPassword, newPassword, bio, link } =
+    req.body;
+  let { profileImg, coverImg } = req.body;
+
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    if (
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
+    ) {
+      return res
+        .status(400)
+        .json({
+          error: "Please provide both current password and new password",
+        });
+    }
+
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch)
+        return res.status(400).json({ error: "Current password is incorrect" });
+      if (newPassword < 6)
+        return res
+          .status(400)
+          .json({ error: "Password must be at least 6 characters long" });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+  } catch (error) {}
+};

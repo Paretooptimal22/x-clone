6d7c0bf67f946883457a9d35313fbc59f767d5e7
kevin young <you.k2208@gmail.com>
2024-09-27 10:37:46 -0700

    deletePost method created in post controller

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 // Router.post("/like/:id", protectRoute, likeUnlikePost);
 // Router.post("/comment/:id", protectRoute, commentOnPost);
-// Router.delete("/", protectRoute, deletePost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;

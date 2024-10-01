460fb9fe79cf3124866707ce8b3d053d632782f0
kevin young <you.k2208@gmail.com>
2024-10-01 08:44:57 -0700

    payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json()); //middleware to parse req.body
+app.use(express.json({ limit: "5mb" })); //middleware to parse req.body
 app.use(express.urlencoded({ extended: true })); //middleware to parse form data(urlencoded)
 app.use(cookieParser()); //middleware to parse cookie
 
